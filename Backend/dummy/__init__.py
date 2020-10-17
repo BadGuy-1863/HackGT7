d1efa9a4b66ccf8739663701c9b8147eeb2e922c
Ayush Nene <ayush3108@gmail.com>
2020-10-17 00:16:34 -0400

    Create dummy backend

---
+++ Backend/dummy/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+def create_app(test_config=None):
+    """
+    Create and configure the app
+    """
+
+    app = Flask(__name__, instance_relative_config=True)
+    
+    @app.route("/")
+    def index():
+        return "Monkey Brainz"
+
+    @app.route("/wait-time/<store_id>", methods=["GET"])
+    def wait_time(store_id):
+        return {
+            store_id: {
+                "store_id": store_id,
+                "wait_time": 10
+            }
+        }
+    return app
