1f0ad25dc893d5b2d0f46ab6bf7f000ce59f22cf
Ayush Nene <ayush3108@gmail.com>
2020-10-17 21:25:20 -0400

    resolve import issue

--- frontend/src/components/MainScreen.tsx
+++ frontend/src/components/MainScreen.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 import Title from "./Title";
 // import { MapBox } from "./MapBox";
 // import { lynn } from "../assets/lynn.png";
 
 const MainScreen = (props: any) => {
     return (
-        <div className='MainScreen'>
+        <div className="MainScreen">
             <Title />
             {/* <MapBox /> */}
         </div>
-    )
-}
+    );
+};
 
-export default MainScreen
+export default MainScreen;
